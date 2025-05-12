27c2eb2ec01d6903fc1f6139896b0684c864eccc
Ryan Wolfslayer <rdw717756@gmail.com>
2025-05-12 18:33:09 -0400

    Update" " g sd

--- scripts/copy_bible_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create the target directory
-mkdir -p assets/data/bible-kjv
-
-# Copy all JSON files from _data/Bible-kjv-master to the new directory
-cp _data/Bible-kjv-master/*.json assets/data/bible-kjv/
-
-echo "Bible KJV data copied to assets/data/bible-kjv/"
